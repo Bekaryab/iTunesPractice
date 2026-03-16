abce7e707d95a86f1cbac7d506e77dfef5a61d08
Bekarys <abievb91@gmail.com>
2026-03-16 16:47:15 +0500

    Initial Commit

---
+++ iTunesPracticeTests/iTunesPracticeTests.swift
@@ -0,0 +1,17 @@
+//
+//  iTunesPracticeTests.swift
+//  iTunesPracticeTests
+//
+//  Created by bekarys abiev on 16.03.2026.
+//
+
+import Testing
+@testable import iTunesPractice
+
+struct iTunesPracticeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
